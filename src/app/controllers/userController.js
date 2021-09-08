ddee504f27273bd461d8054c18af696689f585fd
Maria Baeta <m.martinsdg@gmail.com>
2021-09-08 13:28:03 -0300

    feat: :sparkles: Implementa a criação do carrinho ao cadastrar um novo cliente

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,9 +1,11 @@
 const { User } = require('../models');
+const openTheOrder = require('./orderedController');
 
 const createdUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const newUser = await User.create({ email, password });
+    openTheOrder(newUser.id);
     return res.status(201).json(newUser);
   } catch (e) {
     res.status(500).json(e.message);
