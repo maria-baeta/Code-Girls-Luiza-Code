da3d19af44a8d1ce267d8e2710a108fcd19494b0
Maria Baeta <m.martinsdg@gmail.com>
2021-09-09 22:48:39 -0300

    feat: :sparkles: Adiciona produto ao carrinho

--- routes.js
+++ routes.js
@@ -4,18 +4,19 @@ const getAllStore = require('./src/app/controllers/storeController');
 const emailValidation = require('./src/app/middlewares/emailValidation');
 const passwordValidation = require('./src/app/middlewares/passwordValidation');
 const repeatEmailValidation = require('./src/app/middlewares/repeatEmailValidation');
-const getAllOrdered = require('./src/app/controllers/orderedController');
+// const getAllOrdered = require('./src/app/controllers/orderedController');
 const addProductToOrdered = require('./src/app/controllers/addProductToOrderedController');
-const getAllOrderedProducts = require('./src/app/controllers/orderedProductsController');
+// const getAllOrderedProducts = require('./src/app/controllers/orderedProductsController');
 const userValidation = require('./src/app/middlewares/userValidation');
+const closeTheOrder = require('./src/app/controllers/closeTheOrder');
+const listOrderedUser = require('./src/app/controllers/listOrderedUser');
 // const productValidation = require('./src/app/middlewares/productValidation');
 
 const router = express.Router();
 
 router.post('/user', emailValidation, passwordValidation, repeatEmailValidation, createdUser);
 router.get('/store', getAllStore);
-router.get('/ordered', getAllOrdered);
-router.get('/ordered-products', getAllOrderedProducts);
 router.post('/product/:id', userValidation, addProductToOrdered);
-
+router.put('/ordered/:id', userValidation, closeTheOrder);
+router.get('/ordered/user', userValidation, listOrderedUser);
 module.exports = router;
