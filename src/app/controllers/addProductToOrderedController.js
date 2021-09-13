65f09fb6524af32800bb3f7f95bfc55c867a8978
Kássia Oliveira <kassiafo@gmail.com>
2021-09-13 13:00:23 -0300

    Refatorando o código e realizando tratativa de abrir um novo pedido para usuário.

--- src/app/controllers/addProductToOrderedController.js
+++ src/app/controllers/addProductToOrderedController.js
@@ -3,14 +3,13 @@ const {
   Product, Ordered, OrderedProduct, User,
 } = require('../models');
 const openTheOrder = require('./openTheOrderController');
-
 // Adiciona produto ao pedido de acordo com dados informados do usuário e id do pedido
 const addProduct = async (id, e, p) => {
   const user = await User.findOne({ where: { email: e, password: p } });
   const idUser = user.id;
   const ordered = await Ordered.findOne({ where: { user_id: idUser, status: 'Aberto' } });
   const product = await Product.findByPk(id);
-  if (product === null) return { message: 'Produto não encontrado', status: 400 };
+  if (product === null) return { message: 'Produto não encontrado.', status: 404 };
   const orderedProduct = await OrderedProduct.findOne({
     where: {
       ordered_id: ordered.id,
@@ -23,11 +22,10 @@ const addProduct = async (id, e, p) => {
       ordered_id: ordered.id,
       product_id: product.id,
     });
-    return { message: 'Produto adicionado ao pedido com sucesso', status: 200 };
+    return { message: 'Produto adicionado ao carrinho com sucesso.', status: 200 };
   }
-  return { message: 'Produto já adicionado', status: 400 };
+  return { message: 'Produto já adicionado.', status: 404 };
 };
-
 // Adiciona produto o pedido
 const addProductToOrdered = async (req, res) => {
   try {
@@ -37,20 +35,16 @@ const addProductToOrdered = async (req, res) => {
     const idUser = user.id;
     const product = await Product.findByPk(id);
     const ordered = await Ordered.findOne({ where: { user_id: idUser, status: 'Aberto' } });
-    if (product === null) return res.status(401).json({ message: 'Produto não encontrado' });
-    // Verifaca status do pedido se o mesmo for "Aberto" adiciona o produto,
+    if (product === null) return res.status(404).json({ message: 'Produto não encontrado.' });
+    // Verifica status do pedido se o mesmo for "Aberto" adiciona o produto,
     // se não, será aberto um novo pedido e adicionado o produto.
     if (ordered === null) {
       openTheOrder(idUser);
-      res.status(200).json({ message: 'Pedido aberto, agora é possivel adicionar os produtos desejados' });
       const response = await addProduct(id, emailUser, passwordUser);
-      if (response) return res.status(response.status).json({ message: response.message });
+      return res.status(response.status).json({ message: response.message });
     }
     const newProductAdd = await addProduct(id, emailUser, passwordUser);
-    if (newProductAdd) {
-      return res
-        .status(newProductAdd.status).json({ message: newProductAdd.message });
-    }
+    if (newProductAdd) res.status(newProductAdd.status).json({ message: newProductAdd.message });
   } catch (e) {
     return res.send(e);
   }
